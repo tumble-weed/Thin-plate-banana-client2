d62a8d0d5a2c39807060c9169b901b363104e17e
tumble-weed <aniketsinghresearch@gmail.com>
2022-11-03 05:19:43 +0000

    correct image error, some asserts

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ def encodeBase64Image(image: Image) -> str:
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
+    return img_str
 def main(args):
     #===============================================
     # SECRETS
@@ -31,6 +31,10 @@ def main(args):
     # RESPONSE
     out = banana.run(api_key, model_key, model_inputs)
     model_outputs = out['modelOutputs']
+    assert isinstance(model_outputs,list),f'expecting response["modelOutputs"] to be a list, found {model_outputs.__class__}'
+    assert len(model_outputs) == 1, f'length of response["modelOutputs"] be 1, found {len(response["modelOutputs"])}'
+    model_outputs = model_outputs[0]
+    print(f'status:{model_outputs["message"]}')
     video_base64 = model_outputs['result']
     with open(args.output_path,'wb') as f:
         f.write(base64.b64decode(video_base64))
